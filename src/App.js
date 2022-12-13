acced86226a5af5d30116c125c785e9023c9232c
Marco Gallardo <marco@linkmg.cl>
2022-12-13 15:46:52 -0300

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Pokemon from './components/pokemon';
+import Home from './view/home';
+import Pokemones from './view/pokemones';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <BrowserRouter>
+    <Navbar />
+
+    <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemones/:id" element={<Pokemon />} />
+          <Route path="/pokemones" element={<Pokemones />} />
+    </Routes>
+
+    </BrowserRouter>
+
     </div>
   );
 }
